0f142e19db8b8b928f43ad89663a61c8d2a67a03
An-Cheng Huang <ancheng@vyatta.com>
2010-09-01 14:55:31 -0700

    more cleanup

--- autogen.sh
+++ autogen.sh
@@ -8,4 +8,8 @@ for rmk in conf/*.rmk; do
   ruby genmk.rb < $rmk > `echo $rmk | sed 's/\.rmk$/.mk/'`
 done
 
+rm -f config.sub config.guess
+ln -s /usr/share/misc/config.sub .
+ln -s /usr/share/misc/config.guess .
+
 exit 0
